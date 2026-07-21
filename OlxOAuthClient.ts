da2a2321a5a266861447f72936872ad0d6aa3205
andreazogarcia <andreazogarcia@gmail.com>
2026-07-21 07:48:35 -0300

    Delete OlxOAuthClient.ts

--- OlxOAuthClient.ts
+++
@@ -1,5 +0,0 @@
-export interface ILogger {
-  info(mensagem: string, contexto?: Record<string, unknown>): void;
-  warn(mensagem: string, contexto?: Record<string, unknown>): void;
-  error(mensagem: string, contexto?: Record<string, unknown>): void;
-}
